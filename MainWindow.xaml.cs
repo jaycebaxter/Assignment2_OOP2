877c2157071d241150e47f87a8ca0a168f2950f5
jayce.baxter <jayce.baxter@proton.me>
2025-10-13 13:32:06 -0400

    added player switching function

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,6 +20,23 @@ namespace Assignment2
         // Initializing random logic that we will use in the "choose current player" button
         Random myRandom = new Random();
 
+        // Function to change players after the current player chooses their spot
+
+        private void ChangePlayer()
+        {
+            if (currentPlayer == "X")
+            {
+                currentPlayer = "O";
+                TxtCurrentPlayer.Text = currentPlayer;
+            }
+
+            else if (currentPlayer == "O")
+            {
+                currentPlayer = "X";
+                TxtCurrentPlayer.Text = currentPlayer;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,46 +45,55 @@ namespace Assignment2
         private void BtnGrid1_Click(object sender, RoutedEventArgs e)
         {
             BtnGrid1.Content = currentPlayer;
+            ChangePlayer();
         }
 
         private void BtnGrid2_Click(object sender, RoutedEventArgs e)
         {
             BtnGrid2.Content = currentPlayer;
+            ChangePlayer();
         }
 
         private void BtnGrid3_Click(object sender, RoutedEventArgs e)
         {
             BtnGrid3.Content = currentPlayer;
+            ChangePlayer();
         }
 
         private void BtnGrid4_Click(object sender, RoutedEventArgs e)
         {
             BtnGrid4.Content = currentPlayer;
+            ChangePlayer();
         }
 
         private void BtnGrid5_Click(object sender, RoutedEventArgs e)
         {
             BtnGrid5.Content = currentPlayer;
+            ChangePlayer();
         }
 
         private void BtnGrid6_Click(object sender, RoutedEventArgs e)
         {
             BtnGrid6.Content = currentPlayer;
+            ChangePlayer();
         }
 
         private void BtnGrid7_Click(object sender, RoutedEventArgs e)
         {
             BtnGrid7.Content = currentPlayer;
+            ChangePlayer();
         }
 
         private void BtnGrid8_Click(object sender, RoutedEventArgs e)
         {
             BtnGrid8.Content = currentPlayer;
+            ChangePlayer();
         }
 
         private void BtnGrid9_Click(object sender, RoutedEventArgs e)
         {
             BtnGrid9.Content = currentPlayer;
+            ChangePlayer();
         }
 
         private void BtnChooseStart_Click(object sender, RoutedEventArgs e)
