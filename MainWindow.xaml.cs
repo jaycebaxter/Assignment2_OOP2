d74765e0076c8d61ed1f1d485936844fa1384fff
jayce.baxter <jayce.baxter@proton.me>
2025-10-13 00:03:00 -0400

    added player randomization and click to claim

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.VisualBasic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,11 +12,14 @@ using System.Windows.Shapes;
 
 namespace Assignment2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // Setting current player to default to nothing
+        public string currentPlayer = "";
+        
+        // Initializing random logic that we will use in the "choose current player" button
+        Random myRandom = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -23,52 +27,63 @@ namespace Assignment2
 
         private void BtnGrid1_Click(object sender, RoutedEventArgs e)
         {
-
+            BtnGrid1.Content = currentPlayer;
         }
 
         private void BtnGrid2_Click(object sender, RoutedEventArgs e)
         {
-
+            BtnGrid2.Content = currentPlayer;
         }
 
         private void BtnGrid3_Click(object sender, RoutedEventArgs e)
         {
-
+            BtnGrid3.Content = currentPlayer;
         }
 
         private void BtnGrid4_Click(object sender, RoutedEventArgs e)
         {
-
+            BtnGrid4.Content = currentPlayer;
         }
 
         private void BtnGrid5_Click(object sender, RoutedEventArgs e)
         {
-
+            BtnGrid5.Content = currentPlayer;
         }
 
         private void BtnGrid6_Click(object sender, RoutedEventArgs e)
         {
-
+            BtnGrid6.Content = currentPlayer;
         }
 
         private void BtnGrid7_Click(object sender, RoutedEventArgs e)
         {
-
+            BtnGrid7.Content = currentPlayer;
         }
 
         private void BtnGrid8_Click(object sender, RoutedEventArgs e)
         {
-
+            BtnGrid8.Content = currentPlayer;
         }
 
         private void BtnGrid9_Click(object sender, RoutedEventArgs e)
         {
-
+            BtnGrid9.Content = currentPlayer;
         }
 
         private void BtnChooseStart_Click(object sender, RoutedEventArgs e)
         {
+            int randomNum = myRandom.Next(1, 3);
+            if (randomNum == 1)
+            {
+                currentPlayer = "X";
+                TxtCurrentPlayer.Text = "X";
+            }
 
+            else
+            {
+                currentPlayer = "O";
+                TxtCurrentPlayer.Text = "O";
+            }
         }
 
         private void BtnReset_Click(object sender, RoutedEventArgs e)
@@ -89,6 +104,9 @@ namespace Assignment2
             TxtXNameInput.Text = "";
             TxtONameInput.Text = "";
 
+            TxtCurrentPlayer.Text = "";
+
+            currentPlayer = "";
         }
 
         private void BtnExit_Click(object sender, RoutedEventArgs e)
