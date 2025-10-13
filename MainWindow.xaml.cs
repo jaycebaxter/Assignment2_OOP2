e8bdd25402a4bb3b45b57c770715e6b9bd5f2732
jayce.baxter <jayce.baxter@proton.me>
2025-10-12 22:59:32 -0400

    cleaned up form and added event handlers

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,80 @@ namespace Assignment2
         {
             InitializeComponent();
         }
+
+        private void BtnGrid1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGrid2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGrid3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGrid4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGrid5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGrid6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGrid7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGrid8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGrid9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnChooseStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            BtnGrid1.Content = "";
+            BtnGrid2.Content = "";
+            BtnGrid3.Content = "";
+            BtnGrid4.Content = "";
+            BtnGrid5.Content = "";
+            BtnGrid6.Content = "";
+            BtnGrid7.Content = "";
+            BtnGrid8.Content = "";
+            BtnGrid9.Content = "";
+
+            TxtXScoreDisplay.Text = "";
+            TxtOScoreDisplay.Text = "";
+
+            TxtXNameInput.Text = "";
+            TxtONameInput.Text = "";
+
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
